893249c3b22b650ca4c1092550a8ae23dfa65e2a
Anirudh Muthuswamy <126427302+anirudh-muthuswamy@users.noreply.github.com>
2025-03-25 19:15:24 -0400

    modified dataloader so that sequence id and sequence number for that sequence id is stored for each item, this will be easier to plot out the inference results, or sanity check the sequences after converting to tensors

--- dataset.py
+++ dataset.py
@@ -3,6 +3,8 @@ import os
 import torch
 import numpy as np
 import cv2
+from collections import OrderedDict
+from itertools import islice
 from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
@@ -57,25 +59,33 @@ class CustomDataset(Dataset):
         self.imgw = 224
 
         # Group by sequence_id and collect valid sequences
-        self.sequences = []
+        self.sequences = OrderedDict({})
         num_sequences_total = 0
+        # for each sequence id
         for seq_id in self.df["sequence_id"].unique():
             seq_data = self.df[self.df["sequence_id"] == seq_id]
             num_sequences = max(len(seq_data) - seq_len + 1, 0)
             num_sequences_total += num_sequences
+            # for each sequence of len=seq_len for that sequence_id
             for i in range(len(seq_data) - seq_len + 1):  # Only full sequences
-                self.sequences.append(seq_data.iloc[i : i + seq_len])
+                self.sequences[(seq_id,i)] = (seq_data.iloc[i : i + seq_len])
 
-        print(f"Total sequences extracted: {num_sequences_total}")
+        print(f"Total sequences extracted: {len(self.sequences)}")
+
+    def get_ith_element(self, od, i):
+        return next(islice(od.items(), i, None))
 
     def __len__(self):
         return len(self.sequences)
 
     def __getitem__(self, idx):
-        seq_batch = self.sequences[idx]  # Get one full sequence
+        seq_batch = self.get_ith_element(self.sequences, idx)
+        sequence_id = seq_batch[0][0]
+        seq_num = seq_batch[0][1]
+        seq_df = seq_batch[1]
         # Extract filepaths and steering angles
-        img_names = seq_batch['filepath'].tolist()
-        angles = torch.tensor(seq_batch['steering_angle'].tolist(), dtype=torch.float32)
+        img_names = seq_df['filepath'].tolist()
+        angles = torch.tensor(seq_df['steering_angle'].tolist(), dtype=torch.float32)
 
         # Read and process images in one go with OpenCV
         images = []
@@ -89,7 +99,7 @@ class CustomDataset(Dataset):
         if self.transform:
             images = torch.stack([self.transform(img) for img in images])
 
-        return images, angles
+        return sequence_id, seq_num, images, angles
     
 def create_train_val_dataset(train_csv_file, 
                               val_csv_file,
@@ -116,10 +126,13 @@ def create_train_val_loader(train_dataset, val_dataset, batch_size=8, shuffle=Fa
     train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=4, prefetch_factor=4, pin_memory=True, shuffle=shuffle)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=4, prefetch_factor=4, pin_memory=True, shuffle=shuffle)
 
-    for inputs, labels in train_loader:
+    print('len of train loader:', len(train_loader))
+    print('len of val loader', len(val_loader))
+
+    for (_, _, inputs, labels) in train_loader:
         print("Batch input shape:", inputs.shape)
         print("Batch label shape:", labels.shape)
-        break 
+        break
 
     return train_loader, val_loader
 
