69e128cd564a59a7f51d89cc43d2d753fd823875
Anirudh Muthuswamy <126427302+anirudh-muthuswamy@users.noreply.github.com>
2025-04-16 15:57:11 -0400

    cleaned up code and added crop method to crop part of image

--- conv_ncp/dataset.py
+++ conv_ncp/dataset.py
@@ -46,7 +46,7 @@ def calculate_mean_and_std(dataset_path):
     return mean, std
 
 class CustomDataset(Dataset):
-    def __init__(self, csv_file, seq_len, imgh=224, imgw=224, step_size=1, transform=None):
+    def __init__(self, csv_file, seq_len, imgh=224, imgw=224, step_size=1, crop=True, transform=None):
         """
         Dataset that extracts continuous sequences from the given DataFrame.
 
@@ -60,6 +60,7 @@ class CustomDataset(Dataset):
         self.imgh = imgh
         self.imgw = imgw
         self.step_size = step_size
+        self.crop = crop
 
         # Group by sequence_id and collect valid sequences
         self.sequences = OrderedDict({})
@@ -79,6 +80,12 @@ class CustomDataset(Dataset):
 
     def get_ith_element(self, od, i):
         return next(islice(od.items(), i, None))
+    
+    def _crop_lower_half(self,img, keep_ratio=0.6):
+        #Crops the bottom `keep_ratio` portion of the image.
+        h = img.shape[0]
+        crop_start = int(h * (1 - keep_ratio))
+        return img[crop_start:, :, :]
 
     def __len__(self):
         return len(self.sequences)
@@ -97,7 +104,9 @@ class CustomDataset(Dataset):
         for img_name in img_names:
             img = cv2.imread(img_name)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img = cv2.resize(img, (self.imgw, self.imgh ))  # Resize directly with OpenCV
+            if self.crop:
+                img = self._crop_lower_half(img)
+            img = cv2.resize(img, (self.imgh, self.imgw ))  # Resize directly with OpenCV
             images.append(img)
 
         # Convert to tensor and normalize in batch
@@ -112,6 +121,7 @@ def create_train_val_dataset(train_csv_file,
                               imgw = 224,
                               imgh = 224,
                               step_size = 32,
+                              crop = True,
                               mean=[0.485, 0.456, 0.406],
                               std=[0.229, 0.224, 0.225]):
     
@@ -120,19 +130,20 @@ def create_train_val_dataset(train_csv_file,
         transforms.Normalize(mean=mean, std=std)
     ])
 
-    train_dataset = CustomDataset(csv_file=train_csv_file, seq_len=seq_len,imgh = imgh, imgw=imgw,
-                                  step_size=step_size,transform=transform)
+    train_dataset = CustomDataset(csv_file=train_csv_file, seq_len=seq_len, imgh = imgh, imgw=imgw,
+                                  step_size=step_size,crop=crop, transform=transform)
     val_dataset = CustomDataset(csv_file=val_csv_file, seq_len=seq_len,imgh = imgh, imgw=imgw,
-                                step_size=step_size,transform=transform)
+                                step_size=step_size,crop=crop, transform=transform)
 
     return train_dataset, val_dataset
 
-def create_train_val_loader(train_dataset, val_dataset, train_sampler=None, val_sampler=None, batch_size=8, shuffle=False):
+def create_train_val_loader(train_dataset, val_dataset, train_sampler=None, val_sampler=None, batch_size=8,
+                            num_workers=4, prefetch_factor=2, pin_memory=True, train_shuffle=False):
 
-    train_loader = DataLoader(train_dataset, sampler=train_sampler, batch_size=batch_size, num_workers=4, prefetch_factor=4, 
-                              pin_memory=True, shuffle=shuffle)
-    val_loader = DataLoader(val_dataset, sampler=val_sampler, batch_size=batch_size, num_workers=4, prefetch_factor=4, 
-                            pin_memory=True, shuffle=shuffle)
+    train_loader = DataLoader(train_dataset, sampler=train_sampler, batch_size=batch_size, num_workers=num_workers, 
+                              prefetch_factor=prefetch_factor,pin_memory=pin_memory, shuffle=train_shuffle)
+    val_loader = DataLoader(val_dataset, sampler=val_sampler, batch_size=batch_size, num_workers=num_workers, 
+                            prefetch_factor=prefetch_factor, pin_memory=pin_memory, shuffle=False)
 
     print('len of train loader:', len(train_loader))
     print('len of val loader', len(val_loader))
@@ -144,44 +155,77 @@ def create_train_val_loader(train_dataset, val_dataset, train_sampler=None, val_
 
     return train_loader, val_loader
 
-def get_loaders_for_training(data_dir, steering_angles_path, step_size, filter, turn_threshold, 
-                                     buffer_before, buffer_after, train_size, save_dir):
+def get_loaders_for_training(data_dir, steering_angles_path, step_size, seq_len, imgh, imgw, filter, turn_threshold, 
+                                     buffer_before, buffer_after, crop=True, train_size=0.8, save_dir='data/csv_files', 
+                                     norm=True, batch_size=16, num_workers=4, prefetch_factor=4, pin_memory=True):
     
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    
+    #preprocesses data
     data_preprocessed_pd = get_preprocessed_data_pd(data_dir, steering_angles_path, filter, turn_threshold, 
-                                     buffer_before, buffer_after, save_dir)
+                                     buffer_before, buffer_after, norm, save_dir)
 
     if filter:
-        train_csv_filename = 'train_ncp_data_filtered.csv'
-        val_csv_filename = 'val_ncp_data_filtered.csv'
+        train_csv_filename = f"train_flt_ncp_tt_{turn_threshold}_bb_{buffer_before}_ba_{buffer_after}.csv"
+        val_csv_filename = f"val_flt_ncp_tt_{turn_threshold}_bb_{buffer_before}_ba_{buffer_after}.csv"
     else:
-        train_csv_filename = 'train_ncp_data.csv'
-        val_csv_filename = 'val_ncp_data.csv'
+        train_csv_filename = f"train_ncp_tt_{turn_threshold}_bb_{buffer_before}_ba_{buffer_after}.csv"
+        val_csv_filename = f"val_ncp_tt_{turn_threshold}_bb_{buffer_before}_ba_{buffer_after}.csv"
 
-    train_dataset_path, val_dataset_path = df_split_train_val(data_preprocessed_pd, save_dir=save_dir,
+    #splits data into train and val
+    train_dataset_path, val_dataset_path = df_split_train_val(data_preprocessed_pd,
                                                               train_csv_filename=train_csv_filename,
                                                               val_csv_filename=val_csv_filename,
+                                                              save_dir=save_dir,
                                                               train_size=train_size)
+    #gets custom train and test pytorch dataset
     train_dataset , val_dataset = create_train_val_dataset(train_csv_file = train_dataset_path,
                                                              val_csv_file = val_dataset_path,
-                                                             step_size=step_size)
-    train_loader, val_loader = create_train_val_loader(train_dataset, val_dataset)
+                                                             seq_len=seq_len, imgh=imgh, imgw=imgw,
+                                                             step_size=step_size, crop=crop)
+    
+    #gets dataloaders from dataset
+    train_loader, val_loader = create_train_val_loader(train_dataset, val_dataset,
+                                                       batch_size=batch_size, 
+                                                       num_workers=num_workers, 
+                                                       prefetch_factor=prefetch_factor,
+                                                       pin_memory=pin_memory)
 
     return train_loader, val_loader
 
 if __name__ == '__main__':
 
+    #preprocessing csv file args
     data_dir = 'data/sullychen/07012018/data'
     steering_angles_txt_path = 'data/sullychen/07012018/data.txt'
-    save_dir = 'data/csv_files'
-    step_size = 32
-    train_size = 0.8
+    save_dir = 'data/csv_files_experimental'
     filter = True
+    norm=True
+
     turn_threshold = 0.06 
     buffer_before = 60 
     buffer_after = 60
+    train_size = 0.8
 
-    get_loaders_for_training(data_dir, steering_angles_txt_path, 
-                                     step_size=step_size, filter=filter,
-                                     turn_threshold=turn_threshold, buffer_before=buffer_before,
-                                     buffer_after=buffer_after, train_size=train_size,
-                                     save_dir=save_dir)
+    #custom pytorch dataset args
+    imgh=224
+    imgw=224
+    step_size = 16
+    seq_len = 32
+    crop=True
+
+    #dataloader args
+    batch_size = 16
+    prefetch_factor = 2
+    num_workers=4
+    pin_memory=True
+
+    get_loaders_for_training(
+        #preprocessing args:
+        data_dir, steering_angles_txt_path, save_dir=save_dir, filter=filter, norm=norm,
+        turn_threshold=turn_threshold, buffer_before=buffer_before, buffer_after=buffer_after, train_size=train_size,
+        #dataset args:
+        imgh=imgh, imgw=imgw, step_size=step_size, seq_len=seq_len, crop=crop, 
+        #dataloader args:
+        batch_size=batch_size, prefetch_factor=prefetch_factor, num_workers=num_workers, pin_memory=pin_memory)
